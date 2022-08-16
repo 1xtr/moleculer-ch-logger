c172469ea4e3ce67721f1ee22940b0bdce398c20
Aleksei Anisimov <i@trendco.dev>
2022-08-16 15:57:42 +0300

    Logger init

---
+++ README.md
@@ -0,0 +1,53 @@
+![Moleculer logo](http://moleculer.services/images/banner.png)
+
+[![NPM version](https://img.shields.io/npm/v/@1xtr/moleculer-ch-logger.svg)](https://www.npmjs.com/package/@1xtr/moleculer-ch-logger) ![NPM Downloads](https://img.shields.io/npm/dw/@1xtr/moleculer-ch-logger) ![NPM License](https://img.shields.io/npm/l/@1xtr/moleculer-ch-logger)
+
+# Moleculer logger for ClickHouse
+
+This is a fork from [native Datadog logger](https://github.com/moleculerjs/moleculer/blob/e62016ea16c5c4e303738a66e3a7429237ea9042/src/loggers/datadog.js) 
+
+#   Description
+
+Easy to save logs to ClickHouse with moleculer
+
+# Install
+
+```bash
+$ npm install @1xtr/moleculer-ch-logger --save
+```
+
+# Usage
+
+```js
+const ClickHouseLogger = require('@1xtr/moleculer-ch-logger')
+
+module.exports = {
+    logger: new ClickHouseLogger({
+      // put here your options
+    })
+}
+```
+# Default options
+
+```js
+const options = {
+  url: 'http://localhost',
+  port: 8123,
+  dbName: 'default',
+  dbUser: 'default',
+  dbPassword: '',
+  dbTableName: 'logs',
+  // use source like single TAG
+  source: process.env.MOL_NODE_NAME || 'moleculer',
+  hostname: hostname(),
+  objectPrinter: (o) => {
+    return inspect(o, {
+      showHidden: false,
+      depth: null,
+      colors: false,
+      breakLength: Number.POSITIVE_INFINITY,
+    })
+  },
+  interval: 10 * 1000,
+}
+```
---
+++ index.js
@@ -0,0 +1,255 @@
+/*
+ * moleculer
+ * Copyright (c) 2019 MoleculerJS (https://github.com/moleculerjs/moleculer)
+ * MIT Licensed
+ */
+
+/**
+ * @typedef {Object} ClickHouseLoggerOptions
+ * @property {string} [url='http://localhost'] Url to your CH server like http://localhost
+ * @property {number} [port=8123] Port to connect to server. Default is 8123
+ * @property {string} [dbName='default'] Database name, default is 'default'
+ * @property {string} [dbUser='default'] Database user, default is 'default'
+ * @property {string} [dbPassword=''] Database user password, default is empty string
+ * @property {string} [dbTableName='logs'] Database table, default is 'logs'
+ * @property {string} [source='moleculer'] Use source like single TAG, its default is 'moleculer' or env MOL_NODE_NAME
+ * @property {string} [hostname='hostname'] Hostname, default is machine hostname 'os.hostname()'
+ * @property {Function} [objectPrinter] Callback function for object printer, default is 'util.inspect()'
+ * @property {number} [interval=10000] Date uploading interval in milliseconds, default is 10000
+ */
+
+'use strict'
+require('isomorphic-fetch')
+const _ = require('lodash')
+const BaseLogger = require('moleculer').Loggers.Base
+const {hostname} = require('os')
+const {inspect} = require('util')
+
+fetch.Promise = Promise
+const isObject = function (o) {
+  return o !== null && typeof o === 'object' && !(o instanceof String)
+}
+
+/**
+ * ClickHouseLogger logger for Moleculer
+ *
+ * @class ClickHouseLogger
+ * @constructor
+ * @extends {BaseLogger}
+ */
+class ClickHouseLogger extends BaseLogger {
+  /**
+   * Creates an instance of ClickHouseLogger.
+   * @param {ClickHouseLoggerOptions} opts
+   * @memberof ClickHouseLogger
+   */
+  constructor(opts= {}) {
+    super(opts)
+    /**
+     * @type {ClickHouseLoggerOptions}
+     */
+    const defaultOptions = {
+      url: 'http://localhost',
+      port: 8123,
+      dbName: 'default',
+      dbUser: 'default',
+      dbPassword: '',
+      dbTableName: 'logs',
+      // use source like single TAG
+      source: process.env.MOL_NODE_NAME || 'moleculer',
+      hostname: hostname(),
+      objectPrinter: (o) => {
+        return inspect(o, {
+          showHidden: false,
+          depth: null,
+          colors: false,
+          breakLength: Number.POSITIVE_INFINITY,
+        })
+      },
+      interval: 10 * 1000,
+    }
+    
+    this.opts = _.defaultsDeep(this.opts, defaultOptions)
+    this.queue = []
+    this.timer = null
+    this.host = `${this.opts.url}:${this.opts.port}`
+    
+  }
+  
+  /**
+   * Initialize logger.
+   *
+   * @param {LoggerFactory} loggerFactory
+   */
+  init(loggerFactory) {
+    super.init(loggerFactory)
+    
+    this.createDB()
+      .then((res) => {
+        console.info(`Database ${this.opts.dbName} created successfully`, res);
+        this.createTable()
+          .then((res) => {
+            console.info(`Table ${this.opts.dbTableName} in db ${this.opts.dbName} created successfully`, res);
+          })
+          .catch(err => {
+            /* istanbul ignore next */
+            // eslint-disable-next-line no-console
+            console.warn(
+              `Unable to create table ${this.opts.dbTableName} in database ${this.opts.dbName}. Error:${err.message}`,
+              err,
+            )
+          })
+      })
+      .catch(err => {
+        /* istanbul ignore next */
+        // eslint-disable-next-line no-console
+        console.warn(
+          `Unable to create database ${this.opts.dbName}. Error:${err.message}`,
+          err,
+        )
+      })
+
+    this.objectPrinter = this.opts.objectPrinter
+      ? this.opts.objectPrinter
+      : o =>
+        inspect(o, {
+          showHidden: false,
+          depth: null,
+          colors: false,
+          breakLength: Number.POSITIVE_INFINITY,
+        })
+    
+    if (this.opts.interval > 0) {
+      this.timer = setInterval(() => this.flush(), this.opts.interval)
+      this.timer.unref()
+    }
+  }
+  
+  /**
+   * Stopping logger
+   */
+  stop() {
+    if (this.timer) {
+      clearInterval(this.timer)
+      this.timer = null
+    }
+    
+    return this.flush()
+  }
+  
+  /**
+   * Generate a new log handler.
+   *
+   * @param {object} bindings
+   */
+  getLogHandler(bindings) {
+    let level = bindings ? this.getLogLevel(bindings.mod) : null
+    if (!level) return null
+    
+    const printArgs = args => {
+      return args.map(p => {
+        if (isObject(p) || Array.isArray(p)) return this.objectPrinter(p)
+        return p
+      })
+    }
+    const levelIdx = BaseLogger.LEVELS.indexOf(level)
+    
+    return (type, args) => {
+      const typeIdx = BaseLogger.LEVELS.indexOf(type)
+      if (typeIdx > levelIdx) return
+      
+      this.queue.push({
+        ts: Date.now(),
+        level: type,
+        msg: printArgs(args).join(' '),
+        bindings,
+      })
+      if (!this.opts.interval) this.flush()
+    }
+  }
+  
+  /**
+   * Flush queued log entries to ClickHouseLogger.
+   */
+  flush() {
+    if (this.queue.length > 0) {
+      const rows = Array.from(this.queue)
+      this.queue.length = 0
+      
+      const data = rows.map(row => JSON.stringify({
+        timestamp: row.ts,
+        level: row.level,
+        message: row.msg,
+        nodeID: row.bindings.nodeID,
+        namespace: row.bindings.ns,
+        service: row.bindings.svc,
+        version: row.bindings.ver ? String(row.bindings.ver) : '',
+        source: this.opts.source,
+        hostname: this.opts.hostname,
+      })).join('\n')
+      
+      return fetch(this.host, {
+        method: 'POST',
+        body: `INSERT INTO ${this.opts.dbTableName} FORMAT JSONEachRow ${data}`,
+        headers: {
+          'X-ClickHouse-Database': this.opts.dbName,
+          'X-ClickHouse-User': this.opts.dbUser,
+          'X-ClickHouse-Key': this.opts.dbPassword,
+        },
+      })
+        .then((/*res*/) => {
+          // console.info("Logs are uploaded to DataDog. Status: ", res.statusText);
+        })
+        .catch(err => {
+          /* istanbul ignore next */
+          // eslint-disable-next-line no-console
+          console.warn(
+            "Unable to upload logs to Datadog server. Error:" + err.message,
+            err
+          );
+        })
+    }
+    
+    return this.broker.Promise.resolve()
+  }
+
+  createDB() {
+    const body = `CREATE DATABASE IF NOT EXISTS ${this.opts.dbName}`
+    return fetch(this.host, {
+      method: 'POST',
+      body,
+      headers: {
+        'X-ClickHouse-User': this.opts.dbUser,
+        'X-ClickHouse-Key': this.opts.dbPassword,
+      },
+    })
+  }
+  
+  createTable() {
+    const body = `CREATE TABLE IF NOT EXISTS ${this.opts.dbTableName} (
+          timestamp DateTime DEFAULT now(),
+          level String,
+          message String,
+          nodeID String,
+          namespace String,
+          service String,
+          version String,
+          source String,
+          hostname String)
+      ENGINE = MergeTree()
+      PARTITION BY timestamp
+      ORDER BY tuple()
+      SETTINGS index_granularity = 8192;`
+    return fetch(this.host, {
+      method: 'POST',
+      body,
+      headers: {
+        'X-ClickHouse-Database': this.opts.dbName,
+        'X-ClickHouse-User': this.opts.dbUser,
+        'X-ClickHouse-Key': this.opts.dbPassword,
+      },
+    })
+  }
+}
+
+module.exports = ClickHouseLogger
